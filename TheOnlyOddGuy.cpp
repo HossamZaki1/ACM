6112d7918fe17afe79be01425b9d04523cc8e8c2
Hossam Zaki <hossamyounes4@gmail.com>
2018-10-23 23:36:35 +0200

    Update TheOnlyOddGuy.cpp

--- TheOnlyOddGuy.cpp
+++ TheOnlyOddGuy.cpp
@@ -4,44 +4,46 @@ using namespace std;
 
 int main()
 {
-	int t = 0;
-	cin >> t;
-	int x = 1;
+	int timesOfCases = 0;
+	cin >> timesOfCases;
+	int numberOfElements;
 	int varr = 0;
 	int search = 0;
 	int count = 0;
-	int result = 0;
+	int result;
 
-	for (int i = 0; i < t; i++) {
+	for (int i = 0; i < timesOfCases; i++) {
 
-		cin >> x;
+		cin >> numberOfElements;
 		int* input;
-		input = new int[x];
+		input = new int[numberOfElements];
 
 		//getting data
-		for (int j = 0; j < x; j++) {
+		for (int j = 0; j < numberOfElements; j++) {
 			cin >> varr;
 			input[j] = varr;
 		}
 
 		
 		//searching for each element
-		for (int k = 0; k < x; k++) {
+		for (int k = 0; k < numberOfElements; k++) {
 			search = input[k];
 			count = 0;
 
 			//looking for search item in the whole array
-			for (int l = 0; l <= (x-1); l++) {
+			for (int l = 0; l < (numberOfElements); l++) {
 				if (search == input[l]) {
 					count++;
 				}
 			}
 
-			if (count % 2 != 0) {
+			if ((count % 2) != 0) {
 				result = search;
+				break;
 			}
 		}
 		cout << result;
+		
 	}
 	return 0;
 }
